d129051a8a03fc68b1ae2320f1a11f4936e3523a
Jakob Skjerning <jakob@mentalized.net>
2017-06-12 09:18:57 +0200

    Don't create stuff we don't need along with scaffolds
    - No more helpers
    - No more view specs
    - No more resource-specific assets

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -2,7 +2,12 @@ Rails.application.config.generators do |g|
   # Don't generate the default scaffold stylesheet
   g.scaffold_stylesheet false
 
+  # We'll manually create helpers when we need them (ie rarely)
+  g.helper false
+
+  # Don't generate view specs, we tend to not use them
+  g.view_specs false
+
   # Don't generate resource-specific assets
-  g.javascripts false
-  g.stylesheets false
+  g.assets false
 end
