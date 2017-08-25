1336e516cfa8cbb98e0e34a9d49765d0a8f58baf
Jakob Skjerning <jakob@mentalized.net>
2017-08-25 11:31:03 +0200

    Make Rails UJS work with Webpack

--- app/assets/packs/scripts/application.js
+++ app/assets/packs/scripts/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+// Make Rails standard helpers (ie data-confirm, data-remote etc) work as
+// expected
+import Rails from 'rails-ujs';
+Rails.start()
