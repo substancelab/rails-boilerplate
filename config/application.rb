07ca39c6fc2faccdd07accf1ac7559aac64f5cbe
Jakob Skjerning <jakob@mentalized.net>
2019-08-20 13:33:11 +0200

    Don't add autoload paths to load path
    From https://edgeguides.rubyonrails.org/upgrading_ruby_on_rails.html#config-add-autoload-paths-to-load-path:
    
    > The new configuration point
    >
    > `config.add_autoload_paths_to_load_path`
    >
    > is true by default for backwards compatibility, but allows you to opt-out from
    > adding the autoload paths to $LOAD_PATH.
    >
    > This makes sense in most applications, since you never should require a file
    > in app/models, for example, and Zeitwerk only uses absolute file names
    > internally.
    >
    > By opting-out you optimize $LOAD_PATH lookups (less directories to check), and
    > save Bootsnap work and memory consumption, since it does not need to build an
    > index for these directories.

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,11 @@ module Boilerplate
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # By opting-out from `add_autoload_paths_to_load_path` you optimize
+    # $LOAD_PATH lookups (less directories to check), and save Bootsnap work and
+    # memory consumption, since it does not need to build an index for these
+    # directories.
+    config.add_autoload_paths_to_load_path = false
   end
 end
