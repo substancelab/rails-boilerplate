5b69fd59b1537f831ae0aaa94f3c474622283df6
Jakob Skjerning <jakob@mentalized.net>
2019-08-21 09:44:28 +0200

    Take ownership of environment files
    While Rails really wants to control the coding style and contents of these
    files, in particular during `rails app:upgrade`, these files are our domain.
    
    We'll be adding changes to them, we'll be modifying them to fit our application,
    so we want them to reflect our application, our coding styles.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,12 @@
+# frozen_string_literal: true
+
 Rails.application.configure do
-  # Verifies that versions and hashed value of the package contents in the project's package.json
+  # Verifies that versions and hashed value of the package contents in the
+  # project's package.json
   config.webpacker.check_yarn_integrity = true
-  # Settings specified here will take precedence over those in config/application.rb.
+
+  # Settings specified here will take precedence over those in
+  # config/application.rb.
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
@@ -16,13 +21,13 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+  if Rails.root.join("tmp", "caching-dev.txt").exist?
     config.action_controller.perform_caching = true
     config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}",
     }
   else
     config.action_controller.perform_caching = false
@@ -38,15 +43,16 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "localhost",
-    :port => 1025
+    :port => 1025,
   }
 
   config.action_mailer.default_url_options = {
     :host => "localhost",
-    :port => 3000
+    :port => 3000,
   }
 
-  # Store uploaded files on the local file system (see config/storage.yml for options).
+  # Store uploaded files on the local file system (see config/storage.yml for
+  # options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
