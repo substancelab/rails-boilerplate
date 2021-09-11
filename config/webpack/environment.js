substancelab/rails-boilerplate

commit 8f95232ac22ec15f15c9babfa685b09834fa40b7

Hotfix PostCSS loader incompatibility
We were seeing

    Invalid options object. PostCSS Loader has been initialized using an options object that does not match the API schema.

        options has an unknown property 'config'. These properties are valid:
        object { postcssOptions?, execute?, sourceMap? }

This patch seems to fix it
https://github.com/rails/webpacker/issues/2784 at least until we can
upgrade properly.

config/webpack/environment.js
@@ -1,3 +1,23 @@
 const { environment } = require('@rails/webpacker')
 
+function hotfixPostcssLoaderConfig (subloader) {
+  const subloaderName = subloader.loader
+  if (subloaderName === 'postcss-loader') {
+    if (subloader.options.postcssOptions) {
+      console.log(
+        '\x1b[31m%s\x1b[0m',
+        'Remove postcssOptions workaround in config/webpack/environment.js'
+      )
+    } else {
+      subloader.options.postcssOptions = subloader.options.config;
+      delete subloader.options.config;
+    }
+  }
+}
+
+environment.loaders.keys().forEach(loaderName => {
+  const loader = environment.loaders.get(loaderName);
+  loader.use.forEach(hotfixPostcssLoaderConfig);
+});
+
 module.exports = environment

commit 8515d70f9f21aec40447b5fa50d09473b6887278

Fix issues introduced in 8f95232

config/webpack/environment.js
@@ -9,15 +9,15 @@ function hotfixPostcssLoaderConfig (subloader) {
         'Remove postcssOptions workaround in config/webpack/environment.js'
       )
     } else {
-      subloader.options.postcssOptions = subloader.options.config;
-      delete subloader.options.config;
+      subloader.options.postcssOptions = subloader.options.config
+      delete subloader.options.config
     }
   }
 }
 
 environment.loaders.keys().forEach(loaderName => {
-  const loader = environment.loaders.get(loaderName);
-  loader.use.forEach(hotfixPostcssLoaderConfig);
-});
+  const loader = environment.loaders.get(loaderName)
+  loader.use.forEach(hotfixPostcssLoaderConfig)
+})
 
 module.exports = environment