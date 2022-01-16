64f4c15d89c7be65473660a76f52a58bd73ddef7
Jakob Skjerning <jakob@mentalized.net>
2022-01-16 19:57:14 +0100

    Bump webpacker to 6.0.0.rc6 for Webpack 5

--- Gemfile
+++ Gemfile
@@ -28,7 +28,7 @@ gem "devise"
 gem "simple_form"
 gem "slim-rails"
 gem "turbolinks"
-gem "webpacker"
+gem "webpacker", "6.0.0.rc6"
 
 # Assets management
 gem "uglifier"
--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-module.exports = function (api) {
-  const validEnv = ['development', 'test', 'production']
-  const currentEnv = api.env()
-  const isDevelopmentEnv = api.env('development')
-  const isProductionEnv = api.env('production')
-  const isTestEnv = api.env('test')
+module.exports = function(api) {
+  var validEnv = ['development', 'test', 'production']
+  var currentEnv = api.env()
+  var isDevelopmentEnv = api.env('development')
+  var isProductionEnv = api.env('production')
+  var isTestEnv = api.env('test')
 
   if (!validEnv.includes(currentEnv)) {
     throw new Error(
@@ -51,6 +51,18 @@ module.exports = function (api) {
         '@babel/plugin-proposal-object-rest-spread',
         {
           useBuiltIns: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-methods',
+        {
+          loose: true
+        }
+      ],
+      [
+        '@babel/plugin-proposal-private-property-in-object',
+        {
+          loose: true
         }
       ],
       [
